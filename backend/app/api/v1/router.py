75ba5f352a136f2ee3c0c06ece4780fc0003bb66
Satyam Sharma <satyam17102003@gmail.com>
2025-08-27 10:18:45 +0530

    Added products router

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from app.api.v1.endpoints import sales
+from app.api.v1.endpoints import products
 
 api_router = APIRouter()
 api_router.include_router(sales.router, prefix="/sales", tags=["sales"])
+api_router.include_router(products.router, prefix = "/products", tags = ["products"])
