4a9cf8af4e8020a6c3d8aa473c21ba01cf341bd9
Satyam Sharma <satyam17102003@gmail.com>
2025-08-27 10:11:42 +0530

    Added Product Table

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -9,3 +9,12 @@ class Sale(Base):
     quantity = Column(Integer, nullable=False)
     price_per_unit = Column(Float, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key = True, index = True)
+    name = Column(String, index = True, nullable = False)
+    description = Column(String, nullable = False)
+    price = Column(Float, nullable = False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
