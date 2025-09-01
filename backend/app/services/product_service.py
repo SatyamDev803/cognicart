2649a4a018b544076727babca9e6dd5c74cc0646
Satyam Sharma <satyam17102003@gmail.com>
2025-09-01 19:40:42 +0530

    Create the update and delete function

--- backend/app/services/product_service.py
+++ backend/app/services/product_service.py
@@ -5,13 +5,22 @@ from typing import List
 from app.db.models import Product
 from app.schemas.product import ProductCreate
 
-async def get_all_products(db: AsyncSession) -> List[Product]:
-    result = await db.execute(select(Product).order_by(Product.id))
-    return result.scalars().all()
-
-async def create_product(db: AsyncSession, product: ProductCreate) -> Product:
+async def create_new_product(db: AsyncSession, product: ProductCreate) -> Product:
     db_product = Product(**product.dict())
     db.add(db_product)
     await db.commit()
     await db.refresh(db_product)
     return db_product 
+
+
+async def get_all_products(db: AsyncSession) -> List[Product]:
+    result = await db.execute(select(Product).order_by(Product.id))
+    return result.scalars().all()
+
+
+async def update_product(db: AsyncSession) -> Product:
+    pass 
+
+
+async def delete_sale(db: AsyncSession) -> None:
+    pass
