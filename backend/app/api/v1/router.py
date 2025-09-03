08b6ec6bcd77344841525a79457c0f25e398bc7a
Satyam Sharma <satyam17102003@gmail.com>
2025-09-03 11:45:12 +0530

    Updates Routing

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -3,5 +3,6 @@ from app.api.v1.endpoints import sales
 from app.api.v1.endpoints import products
 
 api_router = APIRouter()
-api_router.include_router(sales.router, prefix="/sales", tags=["sales"])
-api_router.include_router(products.router, prefix = "/products", tags = ["products"])
+
+api_router.include_router(sales.router)
+api_router.include_router(products.router)
