8e706ec781e1a54963ee61e85ec1cd36bb2230ee
Satyam Sharma <satyam17102003@gmail.com>
2025-09-13 21:36:34 +0530

    Updated as per user module

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,6 +5,10 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     DATABASE_URL: str
 
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
     model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
