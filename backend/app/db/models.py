e20f0a295c58afed6515302e03156be64e8c9b48
Satyam Sharma <satyam17102003@gmail.com>
2025-09-18 15:19:44 +0530

    Added User data model

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,38 +1,45 @@
-from time import timezone
-from xmlrpc.client import Boolean
-from sqlalchemy import Column, Integer, String, Float, DateTime, func, CheckConstraint
+from sqlalchemy import (
+    Column, Integer, String, Float, DateTime, func, 
+    CheckConstraint, Boolean, ForeignKey
+)
+from sqlalchemy.orm import relationship
 from app.db.base import Base
 
 class Sale(Base):
     __tablename__ = "sales"
 
     id = Column(Integer, primary_key=True, index=True)
-    product_id = Column(String, index=True, nullable=False)
+    product_id = Column(Integer, ForeignKey("products.id"), nullable=False)
     quantity = Column(Integer, nullable=False)
     price_per_unit = Column(Float, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
+    product = relationship("Product")
+
 class Product(Base):
     __tablename__ = "products"
 
-    id = Column(Integer, primary_key = True, index = True)
-    name = Column(String, index = True, nullable = False)
-    description = Column(String, nullable = False)
-    price = Column(Float, nullable = False)
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True, nullable=False)
+    description = Column(String, nullable=False)
+    price = Column(Float, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
 class User(Base):
-    __tablename = "users"
+    __tablename__ = "users"
 
-    email = Column(String, unique = True, index = True, nullable = True)
-    phone_number = Column(String, unique = True, index = True, nullable = True)
-    hashed_password = Column(String, nullable = True)
-    google_id = Column(String, unqiue = True, nullable = True)
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index = True, nullable = True)
 
-    role = Column(String, nullable = False, default = "viewer")
-    is_active = Column(Boolean, default = True)
-    created_at = Column(DateTime(timezone = True), server_default = func.now())
+    email = Column(String, unique=True, index=True, nullable=True)
+    phone_number = Column(String, unique=True, index=True, nullable=True)
+    hashed_password = Column(String, nullable=True)
+    google_id = Column(String, unique=True, nullable=True)
 
-    __table_args__ = (CheckConstraint('email IS NOT NULL OR phone_number IS NOT NULL', name = 'user_identity_check'))
+    role = Column(String, nullable=False, default="viewer")
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
 
-    
+    __table_args__ = (
+        CheckConstraint('email IS NOT NULL OR phone_number IS NOT NULL', name='user_identity_check'),
+    )
