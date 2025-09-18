fd0e2c03720672cb9784e4e0bbf86c40658cce72
Satyam Sharma <satyam17102003@gmail.com>
2025-09-18 15:29:18 +0530

    Added Auth Router

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import sales
-from app.api.v1.endpoints import products
+from app.api.v1.endpoints import sales, products, auth
 
 api_router = APIRouter()
 
+api_router.include_router(auth.router)
 api_router.include_router(sales.router)
 api_router.include_router(products.router)
