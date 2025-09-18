f0410c80235e6eb5147bb5e84ff20a649fe1c6e1
Satyam Sharma <satyam17102003@gmail.com>
2025-09-18 15:24:59 +0530

    Added Frontend URL and Google variables

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,9 +5,15 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     DATABASE_URL: str
 
+    FRONTEND_URL: str = "http://localhost:3000"
+
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    GOOGLE_CLIENT_ID: str
+    GOOGLE_CLIENT_SECRET: str
+    GOOGLE_REDIRECT_URI: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
