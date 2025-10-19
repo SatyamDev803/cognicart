321aee244627eb60c3305e5c73eedef69ea76411
Satyam Sharma <satyam17102003@gmail.com>
2025-10-19 13:49:29 +0530

    Added Top Product Card

--- frontend/src/app/dashboard/page.js
+++ frontend/src/app/dashboard/page.js
@@ -1,15 +1,18 @@
 "use client";
 
+import { DashboardPageLayout } from "@/components/dashboard-page-layout";
 import { ChartAreaInteractive } from "@/components/chart-area-interactive";
 import { SectionCards } from "@/components/section-cards";
-import { DashboardPageLayout } from "@/components/dashboard-page-layout";
+import { TopProductsCard } from "@/components/top-products-card";
 
 export default function DashboardPage() {
   return (
     <DashboardPageLayout>
       <SectionCards />
-      <div className="px-4 lg:px-6">
+      
+      <div className="grid gap-6 lg:grid-cols-1">
         <ChartAreaInteractive />
+        <TopProductsCard />
       </div>
     </DashboardPageLayout>
   );
