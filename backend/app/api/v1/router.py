eb212fb54213e2e70e2beec5011e22103e12a25e
Satyam Sharma <satyam17102003@gmail.com>
2025-10-19 13:44:46 +0530

    Added route for analytics

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import sales, products, auth
+from app.api.v1.endpoints import sales, products, auth, analytics
 
 api_router = APIRouter()
 
 api_router.include_router(auth.router)
 api_router.include_router(sales.router)
 api_router.include_router(products.router)
+api_router.include_router(analytics.router)
